2dceaa4f253eed32ab50357b5b0ec20ef9691e60
Kevin Toro <ktoro1251@gmail.com>
2023-03-30 00:27:28 -0400

    conditional kotlin

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,10 +12,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
+        val myFirstDice = Dice(6)
+        val rollResult = myFirstDice.roll()
+        val luckyNumber = 4
+
 
         rollButton.setOnClickListener {
             rollDice()
         }
+        //Conditional statement for potential rolls
+        when (rollResult) {
+            luckyNumber -> println("You won!")
+            1 -> println("So sorry! You rolled a 1. Try again!")
+            2 -> println("Sorry, you rolled a 2. Try again!")
+            3 -> println("unfortunately, you rolled a 3. Try again!")
+            5 -> println("Don't cry! you rolled a 5. Try again!")
+            6 -> println("Apologies! You rolled a 6. Try again!")
+        }
+
+
     }
 
     private fun rollDice() {
